58bc1c1ee67a4dbde724bc831cd7157cf7b0bb89
dongliang <liang.dong01@liulishuo.com>
2020-10-13 16:37:34 +0800

    test: 增加 useSetState 单元测试

---
+++ src/useSetState/__test__/index.test.ts
@@ -0,0 +1,44 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import useSetState from "../index";
+
+const setUp = (initialState?: object) =>
+  renderHook(() => useSetState(initialState));
+
+it("it should init state and setter", () => {
+  const { result } = setUp({ foo: "bar" });
+
+  const [state, setState] = result.current;
+
+  expect(state).toEqual({ foo: "bar" });
+  expect(setState).toBeInstanceOf(Function);
+});
+
+it("should init empty state if not initial state provided", () => {
+  const { result } = setUp();
+
+  expect(result.current[0]).toEqual({});
+});
+
+it("should merge changes into current state when providing object", () => {
+  const { result } = setUp({ foo: "bar", count: 1 });
+  const [state, setState] = result.current;
+
+  act(() => {
+    // @ts-ignore
+    setState({ count: state.count + 1, someBool: true });
+  });
+
+  expect(result.current[0]).toEqual({ foo: "bar", count: 2, someBool: true });
+});
+
+it("should merge changes into current state when providing function", () => {
+  const { result } = setUp({ foo: "bar", count: 1 });
+  const [, setState] = result.current;
+
+  act(() => {
+    // @ts-ignore
+    setState((prevState) => ({ count: prevState.count + 1, someBool: true }));
+  });
+
+  expect(result.current[0]).toEqual({ foo: "bar", count: 2, someBool: true });
+});
