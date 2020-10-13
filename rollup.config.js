41a530235a1a672d9da547c534e5d2a3a6d331f9
dongliang <liang.dong01@liulishuo.com>
2020-10-13 16:32:03 +0800

    build: 优化构建流程

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // const json = require("@rollup/plugin-json");
 const { cleandir } = require("rollup-plugin-cleandir");
-// const { nodeResolve } = require("@rollup/plugin-node-resolve");
 import typescript from "rollup-plugin-typescript2";
 import babel from "@rollup/plugin-babel";
 
@@ -12,21 +11,8 @@ module.exports = {
     format: "cjs",
   },
   plugins: [
-    // cleandir("./lib"),
+    cleandir("./lib"),
     typescript(),
     babel({ babelHelpers: "bundled" }),
-    //     typescript({
-    //       tsconfigOverride: {
-    //         compilerOptions: {
-    //           module: "ESNext",
-    //         },
-    //       },
-    //     }),
-    //     nodeResolve({
-    //       extensions,
-    //       modulesOnly: true,
-    //       preferredBuiltins: false,
-    //     }),
-    //     json(),
   ],
 };
